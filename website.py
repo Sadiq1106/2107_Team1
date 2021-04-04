b429de1f976d0f3a613b3cc90c3c72edebd848d1
jiaengoh <57712490+jiaengoh@users.noreply.github.com>
2021-04-04 23:50:39 +0800

    Revert "dropdownz"
    This reverts commit 979522ef3b15a7c149d35045a0d91e147dace270.

--- website.py
+++ website.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
 
-# cd C:\Users\sadiq\Documents\Study Material\Y2T2\2107 Distributed Systems Programming\ICT2107_Project
-# cd C:\Users\sadiq\Desktop\2107_Team1
-# python website.py
+#cd C:\Users\sadiq\Documents\Study Material\Y2T2\2107 Distributed Systems Programming\ICT2107_Project
+#cd C:\Users\sadiq\Desktop\2107_Team1
+#python website.py
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def login():
-    return render_template('home.html')
+	return render_template('home.html')
 
+if __name__=='__main__':
+	app.run(debug=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+	
